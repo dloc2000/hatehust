42ff9d991689861375d4c83b67e708b7f6f07493
dloc2000 <xuanloc176@gmail.com>
2021-05-10 18:37:17 +0700

    Assignment 1

--- SQL/assignment 1/Testing_System6.sql
+++ SQL/assignment 1/Testing_System6.sql
@@ -2,7 +2,7 @@ DROP PROCEDURE IF EXISTS sp_GetDepNameFromDepID;
 DELIMITER $$
 CREATE PROCEDURE sp_GetDepNameFromDepID(IN in_depID TINYINT, OUT in_depName VARCHAR(50))
 BEGIN
-    DECLARE dep_name VARCHAR(50) DEFAULT 0 ;
+   
 	SELECT d.DepartmentName INTO in_depName FROM department d 
 	WHERE d.DepartmentID = in_depID;
 END$$
@@ -11,7 +11,7 @@ DELIMITER ;
 SET @dep_name= '';
  
 
-Call sp_GetDepNameFromDepID(6, @dep_name );
+Call sp_GetDepNameFromDepID('8', @dep_name );
 
 
  SELECT @dep_name;
@@ -26,13 +26,8 @@ BEGIN
     JOIN department d ON a.DepartmentID = d.DepartmentID
 	WHERE d.departmentName = in_depName;
 END$$
-
+DELIMITER ;
 Call sp_GetAccountByDep('Sale');
-
-
-
- 
- 
 -- Question 2: Tạo store để in ra số lượng account trong mỗi group
 DROP PROCEDURE IF EXISTS sp_GetAccountFromGroup;
 DELIMITER $$
@@ -42,11 +37,223 @@ BEGIN
     INNER JOIN `group` g
     WHERE g.GroupName = in_groupName;
 END$$
+DELIMITER ;
 Call sp_GetAccountFromGroup('Testing System');
 
 -- Question 3: Tạo store để thống kê mỗi type question có bao nhiêu question được tạo trong tháng hiện tại
-SELECT * FROM 
-
-
-
+DROP PROCEDURE IF EXISTS sp_GetCountTypeInMonth;
+DELIMITER $$
+CREATE PROCEDURE sp_GetCountTypeInMonth()
+BEGIN
+	SELECT * FROM typequestion tq
+	INNER JOIN `question` q ON q.TypeID = tq.TypeID
+	WHERE month(q.CreateDate) = month(now()) AND year(q.CreateDate) = year(now())
+	GROUP BY q.TypeID;
+END$$
+DELIMITER;
+Call sp_GetCountTypeInMonth();
 -- Question 4: Tạo store để trả ra id của type question có nhiều câu hỏi nhất
+DROP PROCEDURE IF EXISTS sp_TypeID_MaxQuestion;
+DELIMITER $$
+CREATE PROCEDURE sp_TypeID_MaxQuestion(OUT Ques_ID TINYINT)
+BEGIN 
+		WITH CTE_CountTypeID AS(
+			SELECT q.TypeID,count(q.TypeID) AS number_type
+			FROM `question` q
+			GROUP BY q.TypeID
+		)
+		SELECT q.TypeID INTO Ques_ID FROM `question` q
+		GROUP BY q.TypeID
+		HAVING count(q.TypeID) = (SELECT max(number_type) FROM CTE_CountTypeID);
+END$$
+DELIMITER ;
+SET @Ques_ID = 0;
+CALL sp_TypeID_MaxQuestion(@Ques_ID);
+SELECT @Ques_ID;
+
+-- Question 5: Sử dụng store ở question 4 để tìm ra tên của type question
+DROP PROCEDURE IF EXISTS sp_findNameByIDTypeQuestion;
+DELIMITER $$
+CREATE PROCEDURE sp_findNameByIDTypeQuestion(OUT type_name VARCHAR(50) )
+
+BEGIN
+	WITH CTE_CountTypeID AS(
+			SELECT q.TypeID,count(q.TypeID) AS number_type
+			FROM `question` q
+			GROUP BY q.TypeID
+		)
+		SELECT tq.TypeName INTO type_name  FROM `question` q
+        INNER JOIN `typequestion` tq ON tq.TypeID = q.TypeID
+		GROUP BY q.TypeID
+		HAVING count(q.TypeID) = (SELECT max(number_type) FROM CTE_CountTypeID);
+END$$
+DELIMITER ;
+SET @type_name = ' ' ;
+CALL sp_findNameByIDTypeQuestion(@type_name);
+SELECT @type_name;
+
+
+
+DROP PROCEDURE IF EXISTS sp_findNameByIDTypeQuestion;
+DELIMITER $$
+CREATE PROCEDURE sp_findNameByIDTypeQuestion()
+BEGIN
+
+	WITH MAX_Count_TypeID AS(
+		SELECT		COUNT(TypeID) AS max_type
+		FROM		Question 
+		GROUP BY	TypeID
+        HAVING COUNT(TypeID) = (SELECT MAX(max_type) FROM (SELECT COUNT(TypeID) AS max_type 
+																	FROM Question 
+                                                                    GROUP BY TypeID) AS Temp)
+				          )
+    SELECT 		TQ.TypeName
+    FROM		Question Q 
+	INNER JOIN 	TypeQuestion TQ ON Q.TypeID = TQ.TypeID
+    GROUP BY 	Q.TypeID
+    HAVING		COUNT(Q.TypeID) = (SELECT * FROM MAX_Count_TypeID);		
+	
+END$$
+DELIMITER ;
+
+
+
+-- Question 6: Viết 1 store cho phép người dùng nhập vào 1 chuỗi và trả về group có tên chứa chuỗi của người dùng nhập vào hoặc trả về user có username chứa
+				-- chuỗi của người dùng nhập vào
+ DROP PROCEDURE IF EXISTS sp_getNameAccOrGroup;
+ DELIMITER $$
+ CREATE PROCEDURE sp_getNameAccOrGroup(IN var_string VARCHAR(50) ,flag TINYINT)
+ BEGIN
+		IF flag = 1 THEN 
+        SELECT g.GroupName FROM `group`g
+        WHERE  g.GroupName LIKE concat('%', var_string , '%');
+        ELSE
+        SELECT a.FullName FROM `account`a
+        WHERE  a.FullName LIKE concat('%', var_string , '%');
+        END IF ;
+        
+ END$$
+ DELIMITER ;
+ CALL sp_getNameAccOrGroup('VTI' , 1);
+
+ 
+ 
+ 
+-- Question 7: Viết 1 store cho phép người dùng nhập vào thông tin fullName, email và trong store sẽ tự động gán:
+				-- username sẽ giống email nhưng bỏ phần @..mail đi
+				-- positionID: sẽ có default là developer
+				-- departmentID: sẽ được cho vào 1 phòng chờ . Sau đó in ra kết quả tạo thành công
+ DROP PROCEDURE IF EXISTS sp_insertAccount;
+ DELIMITER $$
+ CREATE PROCEDURE sp_insertAccount(IN in_fullName VARCHAR(50) , IN in_Email VARCHAR(50))
+ BEGIN
+		DECLARE Username     VARCHAR(50) DEFAULT (in_Email , '@' , 1);
+        DECLARE PositionID   TINYINT UNSIGNED DEFAULT PositionID = 1;
+		DECLARE departmentID TINYINT UNSIGNED DEFAULT departmentID = 12;
+		DECLARE CreateDate   DATETIME DEFAULT NOW();
+        INSERT INTO `Account` 	(Email		,Username, FullName		, DepartmentID,	PositionID,	CreateDate)
+        VALUE					(in_email	,Username, in_fullName	, departmentID, PositionID, CreateDate);
+	
+        SELECT * FROM `account` a
+        WHERE a.Username = Username ;
+ END$$
+ DELIMITER ;
+ CALL sp_insertAccount('abc111' , 'abc@gmail.com');
+-- Question 8: Viết 1 store cho phép người dùng nhập vào Essay hoặc Multiple-Choice 
+-- để thống kê câu hỏi essay hoặc multiple-choice nào có content dài nhất
+DROP PROCEDURE IF EXISTS sp_maxContentWithTypeName; 
+DELIMITER $$
+CREATE PROCEDURE sp_maxContentWithTypeName(IN in_TypeName VARCHAR(50))
+BEGIN
+	   IF(in_TypeName = 'Essay')    			 THEN
+		   SELECT content , MAX(length(content)) FROM question
+		   WHERE TypeID = 1;
+	   ELSEIF(in_TypeName = 'Multiple-Choice')   THEN
+		   SELECT content , MAX(length(content)) FROM question
+		   WHERE TypeID = 2;
+		END IF;
+END$$
+DELIMITER ;
+CALL sp_maxContentWithTypeName('Multiple-Choice');
+-- Question 9: Viết 1 store cho phép người dùng xóa exam dựa vào ID
+DROP PROCEDURE IF EXISTS sp_DeleteExamOfID;
+DELIMITER $$
+CREATE PROCEDURE sp_DeleteExamOfID(IN in_idExam TINYINT UNSIGNED)
+BEGIN
+      DELETE FROM exam
+      WHERE ExamID = in_idExam;
+END$$
+DELIMITER ;
+CALL sp_DeleteExamOfID('11');
+-- Question 10: Tìm ra các exam được tạo từ 3 năm trước và xóa các exam đó đi (sử dụng store ở câu 9 để xóa)
+				-- Sau đó in số lượng record đã remove từ các table liên quan trong khi removing
+DROP PROCEDURE IF EXISTS sp_delExam3YearAgo()
+DELIMITER $$
+CREATE PROCEDURE sp_delExam3YearAgo(IN in_idExam TINYINT UNSIGNED)
+BEGIN
+	  WITH CTE_Exam3YearAgo 
+      AS(
+			SELECT ExamID
+            FROM   exam
+            WHERE (YEAR(NOW() - YEAR(CreateDate)) > 3
+      )
+      DELETE
+      FROM 	    exam
+      WHERE 	ExamID = (SELECT * FROM CTE_Exam3YearAgo)
+END$$
+DELIMITER ;
+ 
+--  Question 11: Viết store cho phép người dùng xóa phòng ban bằng cách người dùng nhập vào tên phòng ban và các account thuộc phòng ban đó sẽ được
+					-- chuyển về phòng ban default là phòng ban chờ việc
+DROP PROCEDURE IF EXISTS SP_DelDepFromName;
+DELIMITER $$
+CREATE PROCEDURE SP_DelDepFromName(IN var_DepartmentName VARCHAR(30))
+BEGIN
+	DECLARE v_DepartmentID VARCHAR(30) ;
+    SELECT D1.DepartmentID   INTO v_DepartmentID FROM department D1 WHERE D1.DepartmentName = var_DepartmentName;
+
+	UPDATE `account` A SET A.DepartmentID  = '11' WHERE A.DepartmentID = v_DepartmentID;
+    
+	DELETE FROM department d WHERE d.DepartmentName = var_DepartmentName;
+END$$
+DELIMITER ;
+
+Call SP_DelDepFromName('Marketing');
+ 
+ 
+-- Question 12: Viết store để in ra mỗi tháng có bao nhiêu câu hỏi được tạo trong năm nay
+ DROP PROCEDURE IF EXISTS sp_questionOfMonth;
+ DELIMITER $$
+ CREATE PROCEDURE sp_questionOfMonth()
+	 BEGIN
+	    SELECT EachMonthInYear.MONTH, COUNT(QuestionID) AS COUNT
+		FROM
+		(
+             SELECT 1 AS MONTH
+             UNION SELECT 2 AS MONTH
+             UNION SELECT 3 AS MONTH
+             UNION SELECT 4 AS MONTH
+             UNION SELECT 5 AS MONTH
+             UNION SELECT 6 AS MONTH
+             UNION SELECT 7 AS MONTH
+             UNION SELECT 8 AS MONTH
+             UNION SELECT 9 AS MONTH
+             UNION SELECT 10 AS MONTH
+             UNION SELECT 11 AS MONTH
+             UNION SELECT 12 AS MONTH
+        ) AS EachMonthInYear
+        LEFT JOIN Question ON EachMonthInYear.MONTH = MONTH(CreateDate)
+		GROUP BY EachMonthInYear.MONTH
+		ORDER BY EachMonthInYear.MONTH ASC;
+	 END$$
+DELIMITER ;
+CALL sp_questionOfMonth() ;
+ 
+ 
+ 
+ -- Question 13: Viết store để in ra mỗi tháng có bao nhiêu câu hỏi được tạo trong 6 tháng gần đây nhất
+				-- (Nếu tháng nào không có thì sẽ in ra là "không có câu hỏi nào trong tháng")
+
+ 
+
+
---
+++ SQL/assignment 1/Testing_System7.sql
@@ -0,0 +1,150 @@
+
+-- Question 1: Tạo trigger không cho phép người dùng nhập vào Group có ngày tạo trước 1 năm trước
+DROP TRIGGER IF EXISTS Trg_notAddGroup ;
+DELIMITER $$
+		CREATE TRIGGER Trg_notAddGroup
+        BEFORE INSERT ON `group`
+        FOR EACH ROW
+        BEGIN
+			IF NEW.CreateDate < DATE_SUB(CURDATE(), INTERVAL 1 YEAR) THEN
+            SIGNAL SQLSTATE '12345'
+            SET MESSAGE_TEXT = " Cant insert data ";
+            END IF;
+        END$$
+DELIMITER ;        
+-- Question 2: Tạo trigger Không cho phép người dùng thêm bất kỳ user nào vào
+-- 				department "Sale" nữa, khi thêm thì hiện ra thông báo "Department
+--  			"Sale" cannot add more user"
+DROP TRIGGER IF EXISTS Trg_notAddUser ;
+DELIMITER $$
+		CREATE TRIGGER Trg_notAddUser
+		BEFORE INSERT ON `account`
+		FOR EACH ROW
+		BEGIN
+			DECLARE v_depID TINYINT;
+            SELECT d.DepartmentID INTO v_depID FROM `department` d WHERE d.DepartmentName = 'Sale';
+				IF (NEW.DepartmentID = v_depID) THEN
+                SIGNAL SQLSTATE '12345'
+                SET MESSAGE_TEXT = 'khong them duoc dau con a ';
+            END IF;    
+       
+        
+        END$$
+DELIMITER ;       
+INSERT INTO `testingsystem`.`account` (Email    ,     Username  , FullName,  DepartmentID,PositionID) 
+				VALUES      	 	  (   2	,        222       ,   222   ,    2 ,    1   );
+
+-- Question 3: Cấu hình 1 group có nhiều nhất là 5 user
+DROP TRIGGER IF EXISTS Trg_maxUserOfGroups ;
+DELIMITER $$
+			CREATE TRIGGER Trg_maxUserOfGroups
+            BEFORE INSERT ON `groupaccount` 
+            FOR EACH ROW
+            BEGIN
+				DECLARE countUser TINYINT DEFAULT 0;
+                SELECT  count(ga.GroupID) INTO countUser  FROM `groupaccount` ga 
+                GROUP BY ga.GroupID;
+                IF(countUser > 5) THEN
+					SIGNAL SQLSTATE '12345'
+					SET MESSAGE_TEXT = 'cant insert user';
+				END IF;    
+            END$$
+DELIMITER ;  
+INSERT INTO  `groupaccount`(GroupID , AccountID,JoinDate)
+	   VALUES              (  '1'  ,  '2'    , now());
+
+-- Question 4: Cấu hình 1 bài thi có nhiều nhất là 10 Question
+DROP TRIGGER IF EXISTS Trg_maxQuesOfExam ;
+DELIMITER $$
+			CREATE TRIGGER Trg_maxQuesOfExam
+            BEFORE INSERT ON `examquestion` 
+            FOR EACH ROW
+            BEGIN
+				DECLARE countQues TINYINT DEFAULT 0;
+                SELECT  count(ExamID) AS ques_of_exam INTO countQues  FROM `examquestion`  
+                GROUP BY QuestionID;
+                IF(countQues < 11) THEN
+					SIGNAL SQLSTATE '12345'
+					SET MESSAGE_TEXT = 'cant insert question for exam';
+				END IF;    
+            END$$
+DELIMITER ;   
+
+
+-- Question 5: Tạo trigger không cho phép người dùng xóa tài khoản có email là admin@gmail.com 
+--             (đây là tài khoản admin, không cho phép user xóa),
+--             còn lại các tài khoản khác thì sẽ cho phép xóa và sẽ xóa tất cả các thông tin liên quan tới user đó
+DROP TRIGGER IF EXISTS Trg_notDelAdmin ;
+DELIMITER $$
+	CREATE TRIGGER Trg_notDelAdmin 
+	BEFORE DELETE ON `account`
+	FOR EACH ROW
+	BEGIN 
+		DECLARE email VARCHAR(50);
+        SELECT a.Email INTO email FROM `account` a WHERE a.AccountID = OLD.AccountID;
+			IF email = 'vti176@gmail.com' THEN
+				SIGNAL SQLSTATE '12345'
+                SET MESSAGE_TEXT = 'cant del this acc';
+            END IF;    
+	END$$
+DELIMITER ;
+ 
+ DELETE FROM `account` WHERE (AccountID = '12');
+ 
+
+
+-- Question 6: Không sử dụng cấu hình default cho field DepartmentID của table Account
+-- hãy tạo trigger cho phép người dùng khi tạo account không điền vào departmentID thì sẽ được phân vào phòng ban "waiting Department"
+ 
+-- Question 7: Cấu hình 1 bài thi chỉ cho phép user tạo tối đa 4 answers cho mỗi question, trong đó có tối đa 2 đáp án đúng.
+ DROP TRIGGER IF EXISTS Trg_insAnsForQues ;
+DELIMITER $$
+	CREATE TRIGGER Trg_insAnsForQues
+	BEFORE INSERT ON `question`
+	FOR EACH ROW
+	BEGIN 
+			
+	END$$
+DELIMITER ;
+ 
+ 
+ 
+-- Question 8: Viết trigger sửa lại dữ liệu cho đúng:
+-- Nếu người dùng nhập vào gender của account là nam, nữ, chưa xác định
+-- Thì sẽ đổi lại thành M, F, U cho giống với cấu hình ở database
+
+
+-- Question 9: Viết trigger không cho phép người dùng xóa bài thi mới tạo được 2 ngày
+DROP TRIGGER IF EXISTS Trg_notDelExam ;
+DELIMITER $$
+	CREATE TRIGGER Trg_notDelExam
+	BEFORE DELETE ON `exam`
+	FOR EACH ROW
+	BEGIN 
+			IF NEW.CreateDate < DATE_SUB(CURDATE(), INTERVAL 2 DAY) THEN 
+             SIGNAL SQLSTATE '12345'
+            SET MESSAGE_TEXT = " Cant del exam  ";
+            END IF;
+	END$$
+DELIMITER ;
+ 
+
+-- Question 10: Viết trigger chỉ cho phép người dùng chỉ được update, delete các question khi question đó chưa nằm trong exam nào
+ 
+-- Question 12: Lấy ra thông tin exam trong đó:
+-- Duration <= 30 thì sẽ đổi thành giá trị "Short time"
+-- 30 < Duration <= 60 thì sẽ đổi thành giá trị "Medium time"
+-- Duration > 60 thì sẽ đổi thành giá trị "Long time"
+
+
+
+-- Question 13: Thống kê số account trong mỗi group và in ra thêm 1 column nữa có tên là the_number_user_amount và mang giá trị được quy định như sau:
+-- Nếu số lượng user trong group =< 5 thì sẽ có giá trị là few
+-- Nếu số lượng user trong group <= 20 và > 5 thì sẽ có giá trị là normal
+-- Nếu số lượng user trong group > 20 thì sẽ có giá trị là higher
+
+
+
+
+-- Question 14: Thống kê số mỗi phòng ban có bao nhiêu user, nếu phòng ban nào không có user thì sẽ thay đổi giá trị 0 thành "Không có User"
+        
